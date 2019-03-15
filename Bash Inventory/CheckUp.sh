bc0cb017b788e8834e0241c324d115f201d7dffb
logn133 <34286088+logn133@users.noreply.github.com>
2019-03-15 00:36:16 -0400

    Add files via upload

--- Bash Inventory/CheckUp.sh
+++ Bash Inventory/CheckUp.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-
-./baseInv.sh >> check.txt
-
-diff ./baseOut.txt ./check.txt >> checked.txt
+dir=$(pwd)
+time=($date)
+bash ./baseInv.sh -c
+unset -v latest
+for file in "$dir"/*; do
+  if [$file == "*Scan.txt"]; then
+    [[ $file -nt $latest ]] && latest=$file
+  fi
+done
+diff "*-Base-Scan.txt" "$latest" >> "Checked-$time.txt"
+wall "Scan completed, please check the new differences file."
